a7f65f4db11d326961a4cee7f31d8f7afc478ea2
mtarnowski <michal.tarnowski03@gmail.com>
2024-10-24 14:25:23 +0200

    Generator done
    Generator is now done. Script generates data for all tables in database.
    
    One thing that can be change is the complex save_to_csv function.
    It is messy becouse of the previous assumptions that methods generating
    data would need to be be generator objects.
    
    When it was done, and the moment of synchronization needed to create
    3 tables with the same Id came, the approach happen to be wrong.
    
    Otherway, the script is working fine and is ready to be used.

--- data-generator.py
+++ data-generator.py
@@ -2,18 +2,41 @@ import csv
 from DataGenerator import TableDataGenerator
 from pathlib import Path
 import json
+from typing import Generator
 
 
-def save_to_csv(data: list[tuple], rows: list[str], filename: Path) -> None:
+def save_to_csv(
+        data_generator: Generator[tuple, None, None]|Generator[tuple[tuple], None, None], 
+        num_rows: int,
+        header: list[str]|list[list[str]], 
+        filename: Path|list[Path]) -> None:
     try:
+        if isinstance(filename, list):
+            with open(filename[0], mode='w', newline='') as file_1:
+                with open(filename[1], mode='w', newline='') as file_2:
+                    with open(filename[2], mode='w', newline='') as file_3:
+                        writer_1 = csv.writer(file_1)
+                        writer_2 = csv.writer(file_2)
+                        writer_3 = csv.writer(file_3)
+                        writer_1.writerow(header[0])
+                        writer_2.writerow(header[1])
+                        writer_3.writerow(header[2])
+                        for data in data_generator(num_rows):
+                            writer_1.writerow(data[0])
+                            writer_2.writerow(data[1])
+                            writer_3.writerow(data[2])
+                
         with open(filename, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(rows)
-            writer.writerows(data)
+            writer.writerow(header)
+            for data in data_generator(num_rows):
+                writer.writerow(data)
+
     except Exception as e:
         Exception(f"Error while saving to {filename}: {e}")
     else:
         print(f"Data successfully saved to {filename}")
+
 
 def load_json_data(filepath: Path) -> dict:
     try:
@@ -26,19 +49,51 @@ def load_json_data(filepath: Path) -> dict:
 DATA_DIRECTORY = Path('data')
 TABLE_STRUCTURES = load_json_data(DATA_DIRECTORY / 'table_structures.json')
 
-def get_data_methods(object):
-    return [method for method in dir(object) if method.endswith('_data')]
+def save_table_data(table_name, table_header, data_generator, num_rows):
+    if isinstance(table_header, list):
+        filename = [DATA_DIRECTORY / f"{name}.csv" for name in table_name]
+    else: filename = DATA_DIRECTORY / f"{table_name}.csv"
+
+    save_to_csv(data_generator, num_rows, table_header, filename)
 
 if __name__ == "__main__":
     tdg = TableDataGenerator(seed=37)
-    METHODS = get_data_methods(tdg)
+
+    save_table_data(
+        table_name='teacher', 
+        table_header=TABLE_STRUCTURES['teacher'].keys(), 
+        data_generator=tdg.generate_teachers_data, 
+        num_rows=250
+    )
+    save_table_data(
+        table_name='student', 
+        table_header=TABLE_STRUCTURES['student'].keys(), 
+        data_generator=tdg.generate_students_data, 
+        num_rows=10_000
+    )
+    save_table_data(
+        table_name='subject', 
+        table_header=TABLE_STRUCTURES['subject'].keys(), 
+        data_generator=tdg.generate_subjects_data,
+        num_rows=len(tdg.get_possible_subjects())
+    )
+    save_table_data(
+        table_name='offer', 
+        table_header=TABLE_STRUCTURES['offer'].keys(), 
+        data_generator=tdg.generate_offer_data,
+        num_rows=250
+    )
+    save_table_data(
+        table_name=['class', 'feedback', 'attendance'],
+        table_header=[
+            TABLE_STRUCTURES['class'].keys(), 
+            TABLE_STRUCTURES['feedback'].keys(), 
+            TABLE_STRUCTURES['attendance'].keys()
+        ],
+        data_generator=tdg.generate_class_feedback_attendance_data,
+        num_rows=100
+    )
     
-    for table_name, table_attr in TABLE_STRUCTURES.items():
-        method_name = next((m for m in METHODS if table_name in m), None)
-        if method_name:
-            method = getattr(tdg, method_name)
-            save_to_csv(
-                method(int(table_attr['num_rows'])),
-                [*table_attr.keys()][1:], 
-                DATA_DIRECTORY/f'{table_name}.csv'
-            )
+    
+    
+            
