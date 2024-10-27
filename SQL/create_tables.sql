234fad060bf0d379a6020aef9f6f8eebaf0897ca
Michał Tarnowski <95852320+tarnowsky@users.noreply.github.com>
2024-10-27 13:08:29 +0100

    table creation sql added (#13)
    * table creation sql added
    
    * Feedback for present only (#11)
    
    * Feedback for present only
    
    Feddback is now for present only students.
    This ensures that, for example, if the class if for 5 students,
    attendance is taken for 3 students, and feedback is given
    for 3 students.
    
    Signed-off-by: mtarnowski <michal.tarnowski03@gmail.com>
    
    * proportions changed
    
    ---------
    
    Signed-off-by: mtarnowski <michal.tarnowski03@gmail.com>
    
    * Multiple offers per teacher (#14)
    
    Teacher now can have multiple offers of classes.
    Also start date and end date variables were added
    so it will be easier to menage time sections in the future.
    
    Small changes were provided in the class generator,
    so it also generates time of the lesson not only its date.
    
    Signed-off-by: mtarnowski <michal.tarnowski03@gmail.com>
    
    * not null provided
    
    * table creation sql added
    
    * not null provided
    
    ---------
    
    Signed-off-by: mtarnowski <michal.tarnowski03@gmail.com>

---
+++ SQL/create_tables.sql
@@ -0,0 +1,43 @@
+CREATE TABLE student (
+	student_id INT PRIMARY KEY NOT NULL,
+	name NVARCHAR(255),
+	surname NVARCHAR(255),
+	age INT
+);
+
+CREATE TABLE subject (
+    subject_id INT PRIMARY KEY NOT NULL,
+    name NVARCHAR(255)
+);
+
+CREATE TABLE offer (
+    offer_id INT PRIMARY KEY NOT NULL,
+    subject_id INT,
+    teacher_id INT,
+    level INT,
+    duration INT,
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id)
+);
+
+CREATE TABLE class (
+    class_id INT PRIMARY KEY NOT NULL,
+    offer_id INT,
+    student_id INT,
+    date DATETIME,
+    FOREIGN KEY (offer_id) REFERENCES offer(offer_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id)
+);
+
+CREATE TABLE attendance (
+    attendance_id INT PRIMARY KEY NOT NULL,
+    class_id INT,
+    present BIT,
+    FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
+
+CREATE TABLE feedback (
+    feedback_id INT PRIMARY KEY NOT NULL,
+    class_id INT,
+    feedback INT,
+    FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
